d00e44a1da2db18f5e87eda4136d63e8284ad3c0
jslomu <61111216+jslomu@users.noreply.github.com>
2020-11-23 16:57:54 +1100

    Create file.js

---
+++ Javascript/1.Basic/1.1 - Comment/file.js
@@ -0,0 +1,12 @@
+/*
+
+Here is my first javascript file showcasing comments in JavaScript.
+This example in particular is a muli-line comment.
+
+*/
+
+// The function below, on loading the HTML document will insert "Hello" in to the element with an ID of "output".
+
+window.onload = function(){
+    document.getElementById("output").innerHTML = "Hello";
+}
