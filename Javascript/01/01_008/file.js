b5d4ccb9e83ac4e4d5b9ee32d1b724be836fb022
John <61111216+jslomu@users.noreply.github.com>
2020-12-10 17:28:49 +1100

    Changes

---
+++ Javascript/01/01_008/file.js
@@ -0,0 +1,13 @@
+function submit() {
+    document.getElementById("result").innerHTML = studlyCapVar + properCamelCase + titleCaseOver;
+}
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
