10844a26bc51f4bc56d29b3669bb041938582d1d
John <61111216+jslomu@users.noreply.github.com>
2020-12-10 16:42:33 +1100

    Update file.js

--- Javascript/01/01_007/file.js
+++ Javascript/01/01_007/file.js
@@ -3,3 +3,13 @@ function greetings() {
     console.log(variable);
     document.getElementById("result").innerHTML = variable;
 }
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
