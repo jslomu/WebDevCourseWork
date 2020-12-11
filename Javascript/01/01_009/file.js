cdd9c8e49c28a8ec7faf58bc33041e3ba9d93e21
John <61111216+jslomu@users.noreply.github.com>
2020-12-11 13:41:30 +1100

    Update file.js

--- Javascript/01/01_009/file.js
+++ Javascript/01/01_009/file.js
@@ -1,8 +1,6 @@
 function calculate() {
-    let firstNumber = document.getElementById("firstNumber").value;
-    let secondNumber = document.getElementById("secondNumber").value;
-    firstNumber.setAttribute("type","number");
-    secondNumber.setAttribute("type","number");
+    let firstNumber = document.getElementById("firstNumber");
+    let secondNumber = document.getElementById("secondNumber");
     let sum = 0;
     sum = firstNumber + secondNumber;
     document.getElementById("result").innerHTML = sum;
