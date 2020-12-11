14a4004a4a23b4dfc45ab06401b0a05d356de5c7
John <61111216+jslomu@users.noreply.github.com>
2020-12-11 17:16:53 +1100

    Update file.js

---
+++ Javascript/01/01_012/file.js
@@ -0,0 +1,6 @@
+function calculate() {
+    let number1 = document.getElementById("firstNumber");
+    let number2 = document.getElementById("secondNumber");
+    let result = number1 / number2;
+    document.getElementById("result").innerHTML = result;
+}
