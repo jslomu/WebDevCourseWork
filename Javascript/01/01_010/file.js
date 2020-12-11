6af3cf639a628045a32d5b68acb62ce4f8dc6fc7
John <61111216+jslomu@users.noreply.github.com>
2020-12-11 17:01:38 +1100

    Update file.js

--- Javascript/01/01_010/file.js
+++ Javascript/01/01_010/file.js
@@ -1,6 +1,6 @@
 function subtract() {
-    let firstNumber = intParse(document.getElementById("firstNumber").value);
-    let secondNumber = intParse(document.getElementById("secondNumber").value);
+    let firstNumber = parseInt(document.getElementById("firstNumber").value);
+    let secondNumber = parseInt(document.getElementById("secondNumber").value);
     let difference = firstNumber - secondNumber;
     document.getElementById("result").innerHTML = difference;
 }
