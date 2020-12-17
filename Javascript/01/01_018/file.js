6c0138e71f6fa009153c65e02735f59390724f83
John Lomu <john.lomu@hawkesbury.nsw.gov.au>
2020-12-18 08:02:09 +1100

    Updates

---
+++ Javascript/01/01_018/file.js
@@ -0,0 +1,6 @@
+function calculate() {
+    let firstNumber = parseInt(document.getElementById("firstNumber").value);
+    let secondNumber = parseInt(document.getElementById("secondNumber").value);
+    let result = firstNumber / secondNumber;
+    document.getElementById("result").innerHTML = result;
+}
