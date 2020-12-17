bbaacc209999c3cccc426497d915a8f7344a0b3d
John <61111216+jslomu@users.noreply.github.com>
2020-12-17 17:48:44 +1100

    Update file.js

--- Javascript/01/01_016/file.js
+++ Javascript/01/01_016/file.js
@@ -1,8 +1,6 @@
 function multiply() {
     let firstNumber = parseFloat(document.getElementById("firstDecimal").value);
     let secondNumber = parseFloat(document.getElementById("secondDecimal").value);
-    console.log(firstNumber);
-    console.log(secondNumber);
     let result = parseFloat(firstNumber * secondNumber);
     document.getElementById("result").innerHTML = result;
 }
