ea3d686e601c1c16b0329f7ccb3e78bfe43846b6
John <61111216+jslomu@users.noreply.github.com>
2020-12-18 13:39:36 +1100

    Update file.js

---
+++ Javascript/01/01_019/file.js
@@ -0,0 +1,15 @@
+function add() {
+    let a, b, c, stringA, stringB, stringC;
+    a = parseInt(document.getElementById("a").value);
+    b = parseInt(document.getElementById("b").value);
+    c = parseInt(document.getElementById("c").value);
+    a += 1;
+    b += 4;
+    c += 12;
+    stringA = "A is equal to " + a;
+    stringB = "A is equal to " + b;
+    stringC = "A is equal to " + c;
+    document.getElementById("resultA").innerHTML = stringA;
+    document.getElementById("resultB").innerHTML = stringB;
+    document.getElementById("resultC").innerHTML = stringC;
+}
