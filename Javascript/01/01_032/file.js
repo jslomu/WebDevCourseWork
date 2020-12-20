d7c906891cdf13dc1c1940388837a962a531fcb4
John Lomu <john.lomu@hawkesbury.nsw.gov.au>
2020-12-20 21:37:39 +1100

    Bracket Notation

---
+++ Javascript/01/01_032/file.js
@@ -0,0 +1,11 @@
+function firstCharacter() {
+    let string = document.getElementById("string").value
+    let index = string.length - 1
+    let result = string[index];
+    document.getElementById("lastChar").innerHTML = result;
+}
+
+
+function loadpage() {
+    document.getElementById("string").addEventListener("keydown", firstCharacter);
+}
