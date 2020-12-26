0a930553dad665eb3d1aee28c5816ab238c8f8a3
John Lomu <soanelomu@gmail.com>
2020-12-26 12:57:44 +1100

    Completed Tasks

---
+++ Javascript/01/01_071/index.js
@@ -0,0 +1,14 @@
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+    else if (val < 5) {
+      return "Smaller than 5";
+    }
+  
+    return "Between 5 and 10";
+}
+
+function submit(number) {
+    document.getElementById("result").innerHTML = testElseIf(number)
+}
