4d8ea762b22e2215b2b69f2378bfc2cb75f90295
John Lomu <soanelomu@gmail.com>
2021-01-04 08:22:03 +1100

    Update index.js

--- Javascript/01/01_107/index.js
+++ Javascript/01/01_107/index.js
@@ -1,5 +1,5 @@
 function parse() {
     let int = parseInt(document.querySelector("input").value);
-    console.log(int);
+    console.log(typeOf(int));
     document.getElementById("result").innerHTML = int;
 }
