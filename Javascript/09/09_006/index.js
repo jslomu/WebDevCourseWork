4318d240365b92214a77582f21665698f9280067
jslomu <61111216+jslomu@users.noreply.github.com>
2021-01-10 22:42:30 +1100

    New Files

--- Javascript/09/09_006/index.js
+++ Javascript/09/09_006/index.js
@@ -1,20 +1,22 @@
 function spinalCase(str) {
-    let stringArray = str.split("");
-    for (let i = 0; i < stringArray.length; i++) {
-        if (stringArray[i].match(/[A-Z]/g)) {
-        let j;
-        (i === 0) ? j = 0 : j = i -1;
-            if (stringArray[j].match(/\S/g)) {
-                let letter = stringArray[i].toLowerCase();
-                stringArray.splice(i, 1);
-                stringArray.splice(i, 0, letter);
-                stringArray.splice(i, 0, "-");
-            }
-        }
+  let stringArray = str.split("");
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i].match(/[A-Z]/g)) {
+      let j;
+      i === 0 ? (j = 0) : (j = i - 1);
+      if (stringArray[j].match(/\S/g)) {
+        let letter = stringArray[i].toLowerCase();
+        stringArray.splice(i, 1);
+        stringArray.splice(i, 0, letter);
+        stringArray.splice(i, 0, "-");
+      }
     }
-    let string = stringArray.join("").toLowerCase();
-    string = string.replace(/[^a-z]/g, "-");
-    string = string.replace(/--/g, "-");
-    string = string.replace(/^\W/g, "");
-    return string;
+  }
+  let string = stringArray.join("").toLowerCase();
+  string = string.replace(/[^a-z]/g, "-");
+  string = string.replace(/--/g, "-");
+  string = string.replace(/^\W/g, "");
+  return string;
 }
+
+module.exports = spinalCase;
