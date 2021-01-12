1bdbe15f3186a6d59c9b357bbbebc59df1486238
John Lomu <soanelomu@gmail.com>
2021-01-12 20:59:23 +1100

    Update files

--- freeCodeCamp/Javascript/09/09_015/index.js
+++ freeCodeCamp/Javascript/09/09_015/index.js
@@ -1,5 +1,18 @@
 function smallestCommons(arr) {
-    return arr[1];
+    arr.sort((a, b) => b - a);
+
+    let count = 0;
+    let newArray = [];
+
+    for (let i = arr[0]; i < arr[1]; i--) {
+        newArray.push(i);
+    }
+
+    for (let i = arr[0]; count < newArray.length; i++) {
+        if (i % )
+    }
+
+    return arr;
 }
 
 module.exports = smallestCommons
--- freeCodeCamp/Javascript/09/09_015/index.test.js
+++ freeCodeCamp/Javascript/09/09_015/index.test.js
@@ -2,7 +2,7 @@ const { TestScheduler } = require('jest')
 const smallestCommons = require('./index')
 
 describe("Smallest Commons Function", () => {
-    test("Returns a number greater than the larger number", () => {
-        expect(smallestCommons([1,3])).toBeGreaterThanOrEqual(3);
+    test("Sort the array from greatest to smallest.", () => {
+        expect(smallestCommons([1,3]).toEqual([3,1]));
     })
 })
