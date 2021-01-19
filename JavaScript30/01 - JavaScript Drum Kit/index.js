d349a1740d2d473fd051ee9ee13703333ec41537
John Lomu <soanelomu@gmail.com>
2021-01-20 07:56:15 +1100

    Completed the project by following the tutorial video
    Only exception was my solution for removing the yellow border around the keys.

--- JavaScript30/01 - JavaScript Drum Kit/index.js
+++ JavaScript30/01 - JavaScript Drum Kit/index.js
@@ -1,13 +1,16 @@
-window.onload = function(){
-    addEventListener('keydown', (event) => {
-        let code = event.keyCode
-        let audio = document.querySelector(`audio[data-key="${code}"]`);
-        console.log(code);
-        if(!audio) return;
-        audio.currentTime = 0;
-        audio.play();
-    });
-}
+window.addEventListener('keydown', (event) => {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+});
+
+window.addEventListener('keyup', (event) => {
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    key.classList.remove('playing');
+})
 
 // module.exports = {
 //     getKeyCode
