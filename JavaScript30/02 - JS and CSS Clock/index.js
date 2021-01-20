846cb9a5c2a4a59bdb48e732e09249fb1d639699
John Lomu <soanelomu@gmail.com>
2021-01-20 18:18:35 +1100

    Completed clock project

--- JavaScript30/02 - JS and CSS Clock/index.js
+++ JavaScript30/02 - JS and CSS Clock/index.js
@@ -1,10 +1,23 @@
-function getSeconds(time) {
-    console.log(time.getSeconds());
-    return time.getSeconds();
+function getTime() {
+    let time = new Date();
+
+    let seconds = time.getSeconds()
+    let secondsPosition = (seconds / 60) * 360 + 90
+    document.querySelector(".second-hand").style.transform = `rotate(${secondsPosition}deg)`
+    
+    let minutes = time.getMinutes()
+    let minutesPosition = (minutes / 60) * 360 + 90
+    document.querySelector(".min-hand").style.transform = `rotate(${minutesPosition}deg)`
+    
+    let hours = time.getHours()
+    let hoursPosition = (hours / 12) * 360 + 90
+    document.querySelector(".hour-hand").style.transform = `rotate(${hoursPosition}deg)`
 }
 
-setInterval(getSeconds(new Date()), 1000);
+window.onload = () => {
+    setInterval(getTime, 1000);
+};
 
 module.exports = {
-    getSeconds
+    getTime
 }
