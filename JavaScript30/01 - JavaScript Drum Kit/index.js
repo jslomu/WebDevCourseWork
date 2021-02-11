d7c07e8702eadcdb071aab4d799b4b3281344c98
John Lomu <soanelomu@gmail.com>
2021-02-12 09:13:05 +1100

    Updates

--- JavaScript30/01 - JavaScript Drum Kit/index.js
+++ JavaScript30/01 - JavaScript Drum Kit/index.js
@@ -1,10 +1,23 @@
 window.addEventListener('keydown', (event) => {
-    console.log(event.key);
-    var key =  document.querySelector(`p[data-key="${event.key}"]`);
+    var key = document.querySelector(`p[data-key="${event.key}"]`);
     key.style.color = "red";
 });
 
+window.addEventListener("keyup", (event) => {
+    var key = document.querySelector(`p[data-key="${event.key}"]`);
+    key.style.color = "white";
+})
 
-// module.exports = {
-//     getKeyCode
-// }
+window.onload = function() {
+    let keys = document.querySelectorAll(".key-item");
+    keys.forEach(() => {
+        this.addEventListener("mousedown", (event) => {
+        var key = document.querySelector(`p[data-key="${event.srcElement.dataset.key}"]`);
+        key.style.color = "red";
+        })
+        this.addEventListener("mouseup", (event) => {
+        var key = document.querySelector(`p[data-key="${event.srcElement.dataset.key}"]`);
+        key.style.color = "white";
+        })
+    })
+}
