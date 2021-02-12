bc0ad38d4e22deac935fa363250fa8c6ddda2cc0
John Lomu <soanelomu@gmail.com>
2021-02-12 18:32:34 +1100

    Update to files

--- JavaScript30/01 - JavaScript Drum Kit/index.js
+++ JavaScript30/01 - JavaScript Drum Kit/index.js
@@ -1,23 +1,43 @@
 window.addEventListener('keydown', (event) => {
-    var key = document.querySelector(`p[data-key="${event.key}"]`);
-    key.style.color = "red";
+    var key = document.querySelector(`div[data-key="${event.key}"]`);
+    if (key) {
+        key.style.color = "red";
+    };
 });
 
 window.addEventListener("keyup", (event) => {
-    var key = document.querySelector(`p[data-key="${event.key}"]`);
-    key.style.color = "white";
+    var key = document.querySelector(`div[data-key="${event.key}"]`);
+    if (key) {
+        key.style.color = "white";
+    };
 })
 
 window.onload = function() {
     let keys = document.querySelectorAll(".key-item");
     keys.forEach(() => {
         this.addEventListener("mousedown", (event) => {
-        var key = document.querySelector(`p[data-key="${event.srcElement.dataset.key}"]`);
-        key.style.color = "red";
-        })
+            var key = document.querySelector(`div[data-key="${event.srcElement.dataset.key}"]`);
+            if (key) {
+                key.style.color = "red";
+            };
+        });
         this.addEventListener("mouseup", (event) => {
-        var key = document.querySelector(`p[data-key="${event.srcElement.dataset.key}"]`);
-        key.style.color = "white";
+            var key = document.querySelector(`div[data-key="${event.srcElement.dataset.key}"]`);
+            if (key) {
+                key.style.color = "white";
+            };
+        });
+        this.addEventListener("touchstart", (event) => {
+            var key = document.querySelector(`div[data-key="${event.srcElement.dataset.key}"]`);
+            if (key) {
+                key.style.color = "red";
+            };
+        })
+        this.addEventListener("touchend", (event) => {
+            var key = document.querySelector(`div[data-key="${event.srcElement.dataset.key}"]`);
+            if (key) {
+                key.style.color = "white";
+            };
         })
     })
 }
