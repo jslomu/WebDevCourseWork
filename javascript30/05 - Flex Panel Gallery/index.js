284010e04f10b315f19752275f597e930610764b
John Lomu <soanelomu@gmail.com>
2021-02-14 15:29:39 +1100

    Update files

--- javascript30/05 - Flex Panel Gallery/index.js
+++ javascript30/05 - Flex Panel Gallery/index.js
@@ -9,13 +9,16 @@ window.onload = function() {
 let animatePanel = function(event) {
     let div = event.target.id;
     let flexGrow = document.getElementById(div).style.flexGrow;
-    console.log(event);
+    let textArray = document.querySelectorAll(`#${div} > p`);
     if (flexGrow != 5) {
         document.getElementById(div).style.flexGrow = 5;
-        // document.querySelector('')
+        textArray[0].style.top = '25%';
+        textArray[2].style.top = '75%';
     }
     else {
         document.getElementById(div).style.flexGrow = 1;
+        textArray[0].style.top = '-25%';
+        textArray[2].style.top = '125%';
     }
 
 }
